1222383797591b9c953532b42de7e3a195484f44
karino2 <hogeika2@gmailcom>
2018-01-27 13:18:36 +0900

    Ver 1.2

--- app/src/main/java/com/livejournal/karino2/amzaffilink/LinkGenActivity.kt
+++ app/src/main/java/com/livejournal/karino2/amzaffilink/LinkGenActivity.kt
@@ -21,7 +21,7 @@ class LinkGenActivity : AppCompatActivity() {
                 throw RuntimeException("Wrong url ${url}")
 
             val asins = pathSegs[1]
-            return """<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=${trackingId}&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=${asins}&bc1=ffffff<1=_top&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr"> </iframe>"""
+            return """<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=${trackingId}&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=${asins}&bc1=ffffff&lt1=_top&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr"> </iframe>"""
         }
     }
 
